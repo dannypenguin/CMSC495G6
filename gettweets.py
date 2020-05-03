641930aeb1cf0867a98b766c207abb37b54ae12a
Frank Truong <truongnfrank@gmail.com>
2020-05-03 15:35:23 -0400

    new getTweet function based on user id not screen name

--- gettweets.py
+++ gettweets.py
@@ -8,22 +8,26 @@ consumer_secret = "veMK7T5audEbTegdB6gEm0uYnnco6dDp1JvEmciGd5vo5LF9Sx"
 access_key = "-"
 access_secret = ""
 
+usernamelist = ["GovLarryHogan", "GavinNewsom", "CDCgov", "WHO", "WhiteHouse", "NIAIDNews"]
+numberOfTweets = 10
+
+# Authorization to consumer key and consumer secret
+auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+# Calling api
+api = tweepy.API(auth)
+
+# hold tweets as python dicts
+data = []
+
 # Function to extract tweets
 def get_tweets02(username):
-     # Authorization to consumer key and consumer secret
-    auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
-    # Calling api
-    api = tweepy.API(auth)
-
-    # hold tweets as python dicts
-    data = []
 
     # Iterate thru tweets
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name = username, tweet_mode = 'extended').items(2):
-        print(tweet.id)
-        print(tweet.user.name)
-        print(tweet.created_at.strftime("%m/%d/%Y, %H:%M:%S"))
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name = username, tweet_mode = 'extended').items(1):
+        #print(tweet.id)
+        print(tweet.user.name +": "+ str(tweet.user.id))
+        #print(tweet.created_at.strftime("%m/%d/%Y, %H:%M:%S"))
         tweet_info = {
                     'tweet_id': tweet.id,
                     'name': tweet.user.screen_name,
@@ -37,3 +41,43 @@ def get_tweets02(username):
                 }
         data.append(tweet_info)
     return data
+
+def testsources():
+    for i in usernamelist: 
+        get_tweets02(i) 
+
+#testsources()
+
+def getTweets():
+    '''
+    user id list: Governor Larry Hogan, Gavin Newsom, CDC, World Health Organization (WHO), 
+    The White House, NIAID News
+    '''
+    
+    useridlist = [2987671552, 11347122, 146569971, 
+                    14499829, 822215673812119553, 59769395]
+
+    # 
+    for userid in useridlist: 
+            # Iterate thru tweets
+        for tweet in tweepy.Cursor(api.user_timeline, user_id = userid, tweet_mode = 'extended').items(numberOfTweets):
+            
+            print(tweet.user.name +": "+ str(tweet.id))
+            #print(tweet)
+            
+            tweet_info = {
+                    'tweet_id': tweet.id,
+                    'name': tweet.user.screen_name,
+                    #'screen_name': tweet.user.screen_name,
+                    #'retweet_count': tweet.retweet_count,
+                    'text': tweet.full_text,
+                    #'time_read_at': datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+                    'created': tweet.created_at.strftime("%m/%d/%Y, %H:%M:%S"),
+                    #'favourite_count': tweet.favorite_count,
+                    'source_url': tweet.source_url
+                }
+            data.append(tweet_info)
+        return data
+        
+
+#getTweets()
